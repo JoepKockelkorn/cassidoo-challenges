b9d90d2cc126309f66552b941ba1936809d8d4f6
Joep Kockelkorn <joepkockelkorn@gmail.com>
2022-07-07 09:28:13 +0200

    chore: add rummikub assignment

--- src/rummikub.ts
+++ src/rummikub.ts
@@ -1,1 +1,10 @@
+/**
+ * The game Rummikub has 106 tiles: 8 sets numbered 1-13, colored red, blue, black, and yellow, and two (2) “wildcard” tiles.
+
+    Write two functions:
+        one that creates a new player’s tray of 14 tiles (repetitions allowed)
+        and one that returns the valid sets from a given tray
+        
+    A set can be either 3 or 4 tiles of the same number (but all different colors), or it can be a “run” (which is three or more consecutive numbers all in the same color). The rules for Rummikub are here if you need more clarification!
+ */
 console.log('test');
